238596c172b19a7172137f7e96207c6342120d18
chuque95 <CDIAZELVIRDILUV@CRECE.USS.EDU.PE>
2021-10-29 01:41:36 -0500

    chd(19-10)

--- DAD PA1/LaiveKardexApp/Formulario/FormProVencimiento.Designer.vb
+++ DAD PA1/LaiveKardexApp/Formulario/FormProVencimiento.Designer.vb
@@ -30,7 +30,9 @@ Partial Class FormProVencimiento
         Me.ProductosVencimientoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.ReporVencimientoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.ProductosVencimientoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
@@ -45,10 +47,10 @@ Partial Class FormProVencimiento
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource2)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "LaiveKardexApp.VencimientoProductos.rdlc"
-        Me.ReportViewer1.Location = New System.Drawing.Point(12, 91)
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 92)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.ServerReport.BearerToken = Nothing
-        Me.ReportViewer1.Size = New System.Drawing.Size(829, 334)
+        Me.ReportViewer1.Size = New System.Drawing.Size(815, 450)
         Me.ReportViewer1.TabIndex = 0
         '
         'ReporVencimientoBindingSource
@@ -61,36 +63,61 @@ Partial Class FormProVencimiento
         '
         'Panel1
         '
-        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
         Me.Panel1.Controls.Add(Me.Button1)
         Me.Panel1.Location = New System.Drawing.Point(12, 12)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(829, 57)
+        Me.Panel1.Size = New System.Drawing.Size(815, 63)
         Me.Panel1.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(19, 12)
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(443, 14)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(116, 41)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Dia Actual"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Text = "El dia de Hoy"
+        Me.Button1.UseVisualStyleBackColor = False
         '
-        'Button2
+        'Button3
         '
-        Me.Button2.Location = New System.Drawing.Point(533, 12)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Aplicar"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button3.Location = New System.Drawing.Point(565, 14)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(113, 41)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = " En una Semana"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button4.Location = New System.Drawing.Point(684, 14)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(111, 41)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "En un Mes"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(17, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(420, 22)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Lista de Productos que estan proximos a Vencer"
         '
         'FormProVencimiento
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(853, 454)
+        Me.ClientSize = New System.Drawing.Size(839, 554)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "FormProVencimiento"
@@ -98,6 +125,7 @@ Partial Class FormProVencimiento
         CType(Me.ReporVencimientoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.ProductosVencimientoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -106,6 +134,8 @@ Partial Class FormProVencimiento
     Friend WithEvents ReporVencimientoBindingSource As BindingSource
     Friend WithEvents ProductosVencimientoBindingSource As BindingSource
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents Button2 As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
 End Class
