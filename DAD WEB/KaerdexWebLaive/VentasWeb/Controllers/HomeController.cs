145bf6727f4f1292b43f190dc0c7977a21f794bf
Manuel VM <manuelvm@vivancompany.com>
2021-12-01 21:02:31 -0500

    MV-01.12

---
+++ DAD WEB/KaerdexWebLaive/VentasWeb/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KardexLaiveWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Usuario SesionUsuario;
+        public ActionResult Home()
+        {
+            if (Session["Usuario"] != null)
+                SesionUsuario = (Usuario)Session["Usuario"];
+            else {
+                SesionUsuario = new Usuario();
+            }
+            try
+            {
+                ViewBag.NombreUsuario = SesionUsuario.Nombres + " " + SesionUsuario.Apellidos;
+                ViewBag.Cargo = SesionUsuario.Cargo;
+                //ViewBag.RolUsuario = SesionUsuario.oRol.Descripcion;
+
+            }
+            catch {
+
+            }
+
+           
+            return View();
+        }
+
+        public ActionResult Salir()
+        {
+            Session["Usuario"] = null;
+            return RedirectToAction("Login", "Login");
+        }
+
+    }
+}
---
+++ DAD WEB/KaerdexWebLaive/VentasWeb/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KardexLaiveWeb.Utilidades;
+
+namespace KardexLaiveWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string usuario, string clave)
+        {
+
+            Usuario ousuario = CD_Usuario.Instancia.ObtenerUsuarios().Where(u => u.NombreUsuario == usuario && u.Clave == clave ).FirstOrDefault();
+
+            if (ousuario == null)
+            {
+                ViewBag.Error = "Usuario o contraseña no correcta";
+                return View();
+            }
+
+            Session["Usuario"] = ousuario;
+
+            return RedirectToAction("Home", "Home");
+        }
+    }
+}
